142ee3633896ca28f9cb822870723bc91f601090
Dhruv34788 <dhruv.barochia34788@gmail.com>
2019-05-02 00:21:48 +0530

    typo : sidebar - email id

--- src/components/sidebar.jsx
+++ src/components/sidebar.jsx
@@ -10,7 +10,7 @@ export default class Sidebar extends Component {
             <div className="text-center">
               <div className="author-img" style={{backgroundImage: 'url(images/about.jpg)'}} />
               <h1 id="colorlib-logo"><a href="index.html">Dhruv Barochiya</a></h1>
-              <span className="email"><i className="icon-mail"></i> Dhruv.barochiya34788@gamil.com</span>
+              <span className="email"><i className="icon-mail"></i> Dhruv.barochiya34788@gmail.com</span>
               <span className="number"><i className="icon-phone"></i> +91-886-600-5687</span>
             </div>
             <nav id="colorlib-main-menu" role="navigation" className="navbar">
